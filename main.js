834e26becc152c2159ab81c4aa58783a4259608c
Tencona <connertennery@gmail.com>
2019-02-24 05:35:48 -0600

    Added error handling, loading previously learned words on startup
    Error handling is fairly basic, and before it wasn't loading any previously learned words, so it was wiping out all knowledge on startup

--- main.js
+++ main.js
@@ -8,16 +8,18 @@ const Group = require('./src/group.js');
 var token = fs.readFileSync('appToken.config', 'utf8');
 if (token === undefined || token === '') throw 'BAD TOKEN';
 
+var ClientStatuses = { 0: 'READY', 1: 'CONNECTING', 2: 'RECONNECTING', 3: 'IDLE', 4: 'NEARLY', 5: 'DISCONNECTED' };
+
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
+	memory.load();
+	console.log(`Loaded with ${memory.data.groups.length} word groups`);
 });
 
 client.on('message', msg => {
 	if (msg.author.username === 'Ginny') return; //Temporary
 	if (msg.content === 'status') {
-		msg.reply(
-			memory.data.groups.map(group => group.id + ' ' + group.count)
-		);
+		msg.reply(memory.data.groups.map(group => group.id + ' ' + group.count));
 		return;
 	}
 	if (msg.content === 'purge channel') {
@@ -35,9 +37,21 @@ client.on('message', msg => {
 		return;
 	}
 	chain.process(msg.content);
+	var s = client;
 	// if (msg.content === 'ping') {
 	// 	msg.reply('pong');
 	// }
+});
+
+client.on('error', error => {
+	console.log(new Date().toString() + ' Error occured:');
+	console.log(`Status: ${client.status} (${ClientStatuses[client.status]})`);
+	console.log(`Uptime: ${client.uptime}`);
+	if (error.name) console.log(`Name: ${error.name}`);
+	if (error.messsage) console.log(`Message: ${error.message}`);
+	if (error.stack) console.log(`Stack: ${error.stack}`);
+
+	if (client.status === 5) client.login(token);
 });
 
 client.login(token);
